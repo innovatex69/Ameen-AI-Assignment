d1f9dfb7156b92f781bef23b656df6c5f1b0537b
innovatex69 <ffofficial1069op@gmail.com>
2025-08-11 14:46:14 +0530

    Update BEAM_SEARCH.cpp

--- BEAM_SEARCH.cpp
+++ BEAM_SEARCH.cpp
@@ -36,3 +36,22 @@ vector<int> beamSearch(Graph &g,int start,function<bool(int)> isGoal,function<in
     }
     return {};
 }
+
+
+int main(){
+    Graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(4,5);
+    g.addEdge(3,5);
+    int goal=5;
+    auto isGoal=[&](int node){return node==goal;};
+    auto heuristic=[&](int node){return abs(goal-node);};
+    int beamWidth=2;
+    auto path=beamSearch(g,0,isGoal,heuristic,beamWidth);
+    for(int n:path)cout<<n<<" ";
+    cout<<"\n";
+    return 0;
+}
