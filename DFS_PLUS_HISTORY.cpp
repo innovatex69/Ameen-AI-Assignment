60b230176c87932d81725864123668df77a9b2c9
innovatex69 <ffofficial1069op@gmail.com>
2025-08-11 14:37:51 +0530

    Update DFS_PLUS_HISTORY.cpp

--- DFS_PLUS_HISTORY.cpp
+++ DFS_PLUS_HISTORY.cpp
@@ -29,3 +29,18 @@ vector<int> dfsWithHistory(Graph &g,int start,function<void(int)> visit){
     dfsWithHistoryUtil(g,start,visited,history,visit);
     return history;
 }
+
+int main(){
+    Graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(4,5);
+    g.addEdge(3,5);
+    auto history=dfsWithHistory(g,0,[](int node){cout<<node<<" ";});
+    cout<<"\nHistory:";
+    for(int n:history)cout<<n<<" ";
+    cout<<"\n";
+    return 0;
+}
