202cee6fb2433463ae3b417e52013db52862c892
innovatex69 <ffofficial1069op@gmail.com>
2025-08-11 14:37:13 +0530

    edited the main function

--- DFS_PLUS_BFS.cpp
+++ DFS_PLUS_BFS.cpp
@@ -51,18 +51,6 @@ void dfsPlusBfs(Graph &g,int start,function<void(int)> visit){
     cout<<"\nBFS:\n";
     bfs(g,start,visit);
 }
-int main(){
-    Graph g(6);
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(1,3);
-    g.addEdge(2,4);
-    g.addEdge(4,5);
-    g.addEdge(3,5);
-    auto history=dfsWithHistory(g,0,[](int node){cout<<node<<" ";});
-    cout<<"\nHistory:";
-    for(int n:history)cout<<n<<" ";
-    cout<<"\n";
-    return 0;
-}
+
+
 
