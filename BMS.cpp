f22cafaf1756e0e62a0f555e702a560a74980ff5
innovatex69 <ffofficial1069op@gmail.com>
2025-08-11 14:24:12 +0530

    Update BMS.cpp

--- BMS.cpp
+++ BMS.cpp
@@ -35,3 +35,23 @@ bool britishMuseumSearch(Graph &g,int start,function<bool(int)> isGoal,vector<in
     }
     return false;
 }
+
+int main(){
+    Graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(4,5);
+    g.addEdge(3,5);
+    int start=0;
+    int goalNode=5;
+    vector<int> path;
+    bool found=britishMuseumSearch(g,start,[&](int node){return node==goalNode;},path);
+    if(found){
+        cout<<"Path found: ";
+        for(int n:path)cout<<n<<" ";
+        cout<<"\n";
+    }else cout<<"No path found\n";
+    return 0;
+}
