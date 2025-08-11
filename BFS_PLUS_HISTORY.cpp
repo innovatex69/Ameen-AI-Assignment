7f00f97982da8399fcc45d3a7b2cf1a498ca2470
innovatex69 <ffofficial1069op@gmail.com>
2025-08-11 14:35:50 +0530

    Update BFS_PLUS_HISTORY.cpp

--- BFS_PLUS_HISTORY.cpp
+++ BFS_PLUS_HISTORY.cpp
@@ -34,3 +34,17 @@ vector<int> bfsWithHistory(Graph &g,int start,function<void(int)> visit){
     }
     return history;
 }
+int main(){
+    Graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(4,5);
+    g.addEdge(3,5);
+    auto history=bfsWithHistory(g,0,[](int node){cout<<node<<" ";});
+    cout<<"\nHistory:";
+    for(int n:history)cout<<n<<" ";
+    cout<<"\n";
+    return 0;
+}
